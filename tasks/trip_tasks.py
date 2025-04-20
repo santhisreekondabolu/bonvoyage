bf7b11ee5f464f00a9529dab8d51351e7c244ab4
santhisreekondabolu <santhisreekondabolu@gmail.com>
2025-04-20 15:28:47 +0530

    Update trip_tasks.py

--- tasks/trip_tasks.py
+++ tasks/trip_tasks.py
@@ -36,7 +36,7 @@ class TripTasks:
             - ✈️ **Flight options(only Indigo) with booking site links** (e.g., from MakeMyTrip) Day1 should include onward flight detail with booking link, last day itierary should include return flight detail with booking link
             - 🚕 Local transport recommendations (with Mid to Luxury range)
             - 🎒 Daily packing tips based on weather
-            - 💰 Full **budget breakdown** at the end for entire trip (stay, food, transport, tickets, etc.)  flight tickets budget (budget breakdown should include both onward and return price added), Give approximate cost of food.transport (mid to luxery range with a lower limit of 2000 per day), stay for entire trip duration
+            - 💰 Full **budget breakdown** at the end for entire trip (stay, food, transport, tickets, etc.)  flight tickets budget (budget breakdown should include only inbound and outbound flight on day1 and lastday each), Give approximate cost of food.transport (mid to luxery range with a lower limit of 2000 per day), stay for entire trip duration
             Format the result in clear Markdown with colorful images:
             - Headings (e.g. ## Day 1, Day2, Day3..., ## Budget Breakdown (not daily but consolidated for entire trip duration )
             - appropriate colorful and appealing images for locations and tips
