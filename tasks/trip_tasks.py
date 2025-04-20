4569787e6017adf6c95cf6f28891bae67b7645c5
santhisreekondabolu <santhisreekondabolu@gmail.com>
2025-04-20 16:12:58 +0530

    Update trip_tasks.py

--- tasks/trip_tasks.py
+++ tasks/trip_tasks.py
@@ -30,8 +30,8 @@ class TripTasks:
             This itinerary must include:
             - 📅 Clear daily structure (Day 1, Day 2, etc.) Day Wise Itinerary
             - 🌤️ Approximate daily weather forecast
-            - 🗺️ Specific places to visit each day (with short why it's special) 
-            - 🍴 Local restaurants or cafés (name ) 
+            - 🗺️ Specific places to visit each day (with short why it's special) Do not provide any links to places
+            - 🍴 Local restaurants or cafés (name ) Do not provide any links to places
             - 🛏️ **Hotel suggestions** (preferably Mid to luxury range, do not provide booking links )
             - ✈️ **Flight options(only Indigo) with booking site links** (e.g., from MakeMyTrip) Day1 should include onward flight detail with booking link, last day itierary should include return flight detail with booking link
             - 🚕 Local transport recommendations (with Mid to Luxury range)
@@ -39,13 +39,13 @@ class TripTasks:
             - 💰 Full **budget breakdown** at the end for entire trip (stay, food, transport, tickets, etc.)  flight tickets budget (budget breakdown should include only inbound and outbound flight on day1 and lastday each), Give approximate cost of food.transport (mid to luxery range with a lower limit of 2000 per day), stay for entire trip duration
             - Must-know travel tips for smooth navigation {self.__tip_section()}
             Format the result in clear Markdown with colorful images:
-            - Headings (e.g. ## Day 1, Day2, Day3..., ## Budget Breakdown (not daily but consolidated for entire trip duration )
+            - Headings (e.g. ## Day 1, Day2, Day3.., ## Budget Breakdown (not daily but consolidated for entire trip duration )
             - appropriate colorful and appealing images for locations and tips
             - Tables for budget and accommodations (if applicable)
             Make it feel exclusive and realistic, include beautiful appealing and appropriate images, — as if you're planning a trip for a premium travel app.               
             """),
             agent=agent,
-            expected_output=f"Complete {trip_duration}-day expanded travel plan with daily schedule, weather conditions,Placed to visit, restaurants, hotel suggestions, flight options, mid to luxury transport recommendations, packing suggestions. budget breakdown for entire trip duration"
+            expected_output=f"Complete {trip_duration}-day expanded travel plan with daily schedule, weather conditions,Placed to visit, restaurants, hotel suggestions, flight options, mid to luxury transport recommendations, packing suggestions. budget breakdown for entire trip duration, travel tips"
         )
 
     def __tip_section(self):
